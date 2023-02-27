a1b8f41f1b8c5c5dfe46c4779067eb99c8fef794
becom1ng <becom1ng@users.noreply.github.com>
2023-02-27 17:52:30 -0600

    Basic design with values from JSON object.

--- main.js
+++ main.js
@@ -8,4 +8,15 @@ var weather = {
        "temp": 98,
        "humidity": 93
      }
- }
+}
+
+function displayWeather(weather) {
+  var elemTemp = document.getElementById('temperature')
+  var elemHumidity = document.getElementById('humidity')
+  var elemWind = document.getElementById('wind')
+  elemTemp.innerText = weather.main.temp;
+  elemHumidity.innerText = weather.main.humidity;
+  elemWind.innerText = weather.wind.speed;
+}
+
+displayWeather(weather);
