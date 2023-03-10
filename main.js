e0d33a5082dc99c96a9438f1ea94bc47a84d0783
becom1ng <becom1ng@users.noreply.github.com>
2023-03-09 19:28:32 -0600

    Refactor plus cleanup

--- main.js
+++ main.js
@@ -8,13 +8,8 @@ function getLocation() {
 }
 
 function showPosition(position) {
-  console.log('Latitude: ' + position.coords.latitude + ' Longitude: ' + position.coords.longitude);
   var fetchURL = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=fd7bb1c5117b8de8488094b4094e66e4&units=imperial`
-  console.log(fetchURL);
-  
-//  fetch(fetchURL)
-//    .then(response => response.json())
-//    .then(data => console.log(data));
+  doFetch(fetchURL);
 }
 
 function showError(error) {
@@ -34,90 +29,97 @@ function showError(error) {
   }
 } 
 
-//getLocation();
+// getLocation(); // Request location of browser
 
-// End default location -->
+// Fetch weather data and pass to update function
+function doFetch(fetchURL) {
+  fetch(fetchURL)
+    .then(response => response.json())
+    .then(data => displayWeather(data));
+}
 
-// fetch takes in an endpoint / url
+// Update page to display weather
+function displayWeather(weather) {
+  var elemTemp = document.getElementById('temperature')
+  var elemFeelsLike = document.getElementById('feelsLike')
+  var elemConditions = document.getElementById('conditions')
+  var elemWind = document.getElementById('wind')
+  var elemWeatherIcon = document.getElementById('weatherIcon')
+  var elemCurrentDisplay = document.getElementById('currentDisplay')
+
+    function getCardinalDirection(angle) {
+      const directions = ['N ↑', 'NE ↗', 'E →', 'SE ↘', 'S ↓', 'SW ↙', 'W ←', 'NW ↖'];
+      return directions[Math.round(angle / 45) % 8];
+    }
+
+  elemCurrentDisplay.innerText = weather.name  
+  elemTemp.innerText = Math.round(weather.main.temp);
+  elemFeelsLike.innerText = Math.round(weather.main.feels_like);
+  elemConditions.innerText = weather.weather[0].main;
+  elemWeatherIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" height="120px"></img>`;
+  elemWind.innerText = Math.round(weather.wind.speed) + " " + getCardinalDirection(weather.wind.deg);
+}
+
+function fetchClick() {
+  var inputRequest = document.getElementById('weatherRequest');
+  console.log(inputRequest.value);
+  
+  // if inputRequest is ##### then fetchWeather(inputRequest)
+  // if inputRequest is string, then split into city name and state
+  //    like pop last 2 characters of the string for state
+  //    then delete and ,
+  //    pass remaining as city name + state extracted earlier
+}
+
+// for testing purpose via console
 function fetchWeather(zip) {
   var fetchURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=fd7bb1c5117b8de8488094b4094e66e4&units=imperial`
-  console.log(fetchURL);
-  
-//  fetch(fetchURL)
-//    .then(response => response.json())
-//    .then(data => console.log(data));
+  doFetch(fetchURL);
 }
-
-// fetchWeather(63119); // Get weather from zip
-
-
-
 
 // Sample weather JSON - PLACEHOLDER TO GENERATE DEFAULT VALUES
-var testweather = {
-  "coord": {
-    "lon": -90.3283,
-    "lat": 38.6007
-  },
-  "weather": [
-    {
-      "id": 800,
-      "main": "Clear",
-      "description": "clear sky",
-      "icon": "01n"
-    }
-  ],
-  "base": "stations",
-  "main": {
-    "temp": 56.32,
-    "feels_like": 54.7,
-    "temp_min": 52.83,
-    "temp_max": 58.42,
-    "pressure": 1006,
-    "humidity": 65
-  },
-  "visibility": 10000,
-  "wind": {
-    "speed": 16.11,
-    "deg": 100
-  },
-  "clouds": {
-    "all": 0
-  },
-  "dt": 1677630197,
-  "sys": {
-    "type": 2,
-    "id": 2008299,
-    "country": "US",
-    "sunrise": 1677587728,
-    "sunset": 1677628359
-  },
-  "timezone": -21600,
-  "id": 4397042,
-  "name": "Maplewood",
-  "cod": 200
-}
+// var testweather = {
+//   "coord": {
+//     "lon": -90.3283,
+//     "lat": 38.6007
+//   },
+//   "weather": [
+//     {
+//       "id": 800,
+//       "main": "Clear",
+//       "description": "clear sky",
+//       "icon": "01n"
+//     }
+//   ],
+//   "base": "stations",
+//   "main": {
+//     "temp": 56.32,
+//     "feels_like": 54.7,
+//     "temp_min": 52.83,
+//     "temp_max": 58.42,
+//     "pressure": 1006,
+//     "humidity": 65
+//   },
+//   "visibility": 10000,
+//   "wind": {
+//     "speed": 16.11,
+//     "deg": 100
+//   },
+//   "clouds": {
+//     "all": 0
+//   },
+//   "dt": 1677630197,
+//   "sys": {
+//     "type": 2,
+//     "id": 2008299,
+//     "country": "US",
+//     "sunrise": 1677587728,
+//     "sunset": 1677628359
+//   },
+//   "timezone": -21600,
+//   "id": 4397042,
+//   "name": "Maplewood",
+//   "cod": 200
+// }
 
-// Primary Functionality
-function displayWeather(weather) {
-var elemTemp = document.getElementById('temperature')
-var elemFeelsLike = document.getElementById('feelsLike')
-var elemConditions = document.getElementById('conditions')
-var elemWind = document.getElementById('wind')
-var elemWeatherIcon = document.getElementById('weatherIcon')
-
-  function getCardinalDirection(angle) {
-    const directions = ['N ↑', 'NE ↗', 'E →', 'SE ↘', 'S ↓', 'SW ↙', 'W ←', 'NW ↖'];
-    return directions[Math.round(angle / 45) % 8];
-  }
-
-  console.log(getCardinalDirection(weather.wind.deg))
-
-elemTemp.innerText = Math.round(weather.main.temp);
-elemFeelsLike.innerText = Math.round(weather.main.feels_like);
-elemConditions.innerText = weather.weather[0].main;
-elemWeatherIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png" height="120px"></img>`;
-elemWind.innerText = Math.round(weather.wind.speed) + " " + getCardinalDirection(weather.wind.deg);
-}
-
-displayWeather(testweather);
+// displayWeather(testweather);
